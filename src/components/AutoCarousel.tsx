4c3734bc3448360c93bed9ebabbb041b425dbd30
Kyle-Close <k.james.close@gmail.com>
2023-08-06 13:52:55 -0700

    making custom carousel

--- src/components/AutoCarousel.tsx
+++ src/components/AutoCarousel.tsx
@@ -1,80 +1,54 @@
-import React, { Component } from "react";
-import img from "../img/javascript.png";
-import ReactDOM from "react-dom";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+import React from "react";
+import "../styles/Carousel/Carousel.css";
+
+import jsCarouselImg from "../img/js-carousel.svg";
+import tsCarouselImg from "../img/ts-carousel.svg";
+import reactCarouselImg from "../img/react-carousel.svg";
+import htmlCarouselImg from "../img/html-carousel.svg";
+import tailwindCarouselImg from "../img/tailwind-carousel.svg";
+import nodeCarouselImg from "../img/node-carousel.svg";
+import gitCarouselImg from "../img/git-carousel.svg";
+import mySQLCarouselImg from "../img/mySQL-carousel.svg";
 
 function AutoCarousel() {
   const [currentSlide, setCurrentSlide] = React.useState<number>(0);
   const [autoPlay, setAutoPlay] = React.useState(true);
 
-  const buttonStyle = { fontSize: 20, padding: "5px 20px", margin: "5px 0px" };
-  const containerStyle = { margin: "5px 0 20px" };
+  function nextSlide() {}
 
-  function next() {
-    setCurrentSlide((prevCurrentSlide) => {
-      return prevCurrentSlide + 1;
-    });
-  }
-
-  function prev() {
-    setCurrentSlide((prevCurrentSlide) => {
-      return prevCurrentSlide - 1;
-    });
-  }
-
-  function changeAutoPlay() {
-    setAutoPlay((prevAutoPlay) => !prevAutoPlay);
-  }
-
-  function updateCurrentSlide(index: number) {
-    setCurrentSlide(index);
-  }
+  let itemStyle = {
+    transform: `translate(-${50 * currentSlide + 5 * currentSlide}px)`,
+  };
 
   return (
-    <div>
-      <div style={containerStyle}>
-        <p style={containerStyle}>
-          Use the buttons below to change the selected item in the carousel
-          <br />
-          <small>
-            <i>
-              Note that the external controls might not respect the carousel
-              boundaries but the carousel won't go past it.
-            </i>
-          </small>
-        </p>
-        <p>External slide value: {currentSlide}</p>
-        <button onClick={prev} style={buttonStyle}>
-          Prev
-        </button>
-        <button onClick={next} style={buttonStyle}>
-          Next
-        </button>
-        <button onClick={changeAutoPlay} style={buttonStyle}>
-          Toggle Autoplay ({autoPlay ? "true" : "false"})
-        </button>
+    <div className="carousel-container">
+      <div className="carousel">
+        <div style={itemStyle} className="item 1">
+          <img src={jsCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 2">
+          <img src={tsCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 3">
+          <img src={reactCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 4">
+          <img src={htmlCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 5">
+          <img src={tailwindCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 6">
+          <img src={nodeCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 7">
+          <img src={gitCarouselImg} />
+        </div>
+        <div style={itemStyle} className="item 8">
+          <img src={mySQLCarouselImg} />
+        </div>
       </div>
-      <Carousel
-        autoPlay={autoPlay}
-        selectedItem={currentSlide}
-        onChange={updateCurrentSlide}
-        infiniteLoop={true}
-        interval={1500}
-      >
-        <div>
-          <img src={img} />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src={img} />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src={img} />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
+      <button>Next</button>
     </div>
   );
 }
