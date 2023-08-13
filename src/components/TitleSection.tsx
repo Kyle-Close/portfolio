eaa55064c4d85bcafb67d6cae0aeb3d87bd4081a
Kyle-Close <k.james.close@gmail.com>
2023-08-13 00:26:23 -0700

    further progress

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -8,14 +8,7 @@ function TitleSection() {
   return (
     <Box sx={titleContainer}>
       <h5 className="intro">Hi, my name is</h5>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-          flexWrap: "wrap",
-        }}
-      >
+      <Box sx={nameAndWebDev}>
         <Typography sx={name} variant="h2" component="h2">
           KYLE CLOSE
         </Typography>
@@ -34,6 +27,14 @@ function TitleSection() {
   );
 }
 
+const nameAndWebDev = {
+  display: "flex",
+  justifyContent: "space-between",
+  gap: "15px",
+  width: "100%",
+  flexWrap: "wrap",
+};
+
 const titleContainer = {
   display: "flex",
   flexWrap: "wrap",
@@ -50,7 +51,7 @@ const name = {
   },
   display: "inline-block", // Explicitly setting display to inline-block
   alignItems: "center",
-  fontWeight: "500",
+  fontWeight: "600",
   fontSize: {
     xs: "1.2rem",
     sm: "1.5rem",
@@ -64,6 +65,7 @@ const webDevContainer = {
 };
 
 const webDev = {
+  opacity: "0.7",
   "::first-letter": {
     fontSize: {
       xs: "1.7rem",
@@ -75,9 +77,9 @@ const webDev = {
   alignItems: "center",
   fontWeight: "500",
   fontSize: {
-    xs: "1.2rem",
-    sm: "1.5rem",
-    md: "1.8rem",
+    xs: "1rem",
+    sm: "1.2rem",
+    md: "1.5rem",
   },
 };
 
