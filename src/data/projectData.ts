72685c98ca70f54d1ec69c02c35f7e6ef70e94c2
kyle-close <k.james.close@gmail.com>
2024-04-16 16:13:10 -0700

    save

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,40 +1,38 @@
 import knightsDark from '../img/knights-dark.png';
 import knightsLight from '../img/knights-light.png';
 
-import snakeDark from '../img/snake-dark.png';
-import snakeLight from '../img/snake-light.png';
-
-import memoryDark from '../img/memory-dark.png';
-import memoryLight from '../img/memory-light.png';
-
 import bitelogDark from '../img/laptop-bitelog-dark.png';
+import bitelogLight from '../img/laptop-bitelog-light.png';
+
+import blogDark from '../img/laptop-blog-dark.png';
+import blogLight from '../img/laptop-blog-light.png';
 
 const ProjectData = [
   {
     name: 'Bitelog',
     techStack: ['React', 'Express', 'Postgres', 'Jest'],
     description:
-      'Bitelog is a dynamic web application designed to empower individuals to track and analyze their dietary habits in relation to their health. Users can create personalized lists of ingredients and meals, log dietary intakes with detailed timestamps, and record health events such as discomforts or reactions following their meals. This platform serves as a powerful tool for identifying food sensitivities, managing diets, and optimizing nutritional health.',
+      'Web application designed to empower individuals to track and analyze their dietary habits in relation to their health. Create personalized lists of ingredients and meals, log dietary intakes with detailed timestamps, and record health events such as discomforts or reactions.',
     source: 'https://github.com/Kyle-Close/bitelog-client',
     live: 'https://kyle-close.github.io/bitelog-client/',
-    imgDark: snakeDark,
-    imgLight: snakeLight,
+    imgDark: bitelogDark,
+    imgLight: bitelogLight,
   },
   {
-    name: 'Memory Mix-Up',
-    techStack: ['Javascript', 'HTML', 'CSS'],
+    name: 'Blogging with Bits',
+    techStack: ['React', 'MongoDB', 'Tailwind'],
     description:
-      'Select a theme for each new game. Navigate through levels by clicking on unique images. Be cautious: clicking an image twice will lead to defeat. Remember, images shuffle after every click.',
-    source: 'https://github.com/Kyle-Close/memory-card-game',
-    live: 'https://kyle-close.github.io/memory-card-game/',
-    imgDark: bitelogDark,
-    imgLight: bitelogDark,
+      'A blogging site for all things technology! Become an author and create and edit your posts. Post and view comments from other users on any blog post.',
+    source: 'https://github.com/Kyle-Close/blog-client',
+    live: 'https://blog-gold-seven-38.vercel.app/',
+    imgDark: blogDark,
+    imgLight: blogLight,
   },
   {
     name: 'Knights-Travails',
     techStack: ['Javascript', 'HTML', 'CSS'],
     description:
-      "A knight in chess can move to any square on the standard 8x8 chess board from any other square on the board, given enough turns. In this application, I've used my knowledge of data structures & algorithms to visualize the shortest path. Choose a starting square and ending square, then click calculate to visualize.",
+      'Shortest path visualizer of a knight in the classic board game, chess.',
     source: 'https://github.com/Kyle-Close/Knights-Travails',
     live: 'https://kyle-close.github.io/Knights-Travails/',
     imgDark: knightsDark,
