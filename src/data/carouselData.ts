2ba0bdee057dc3fe40db65db1eabdd9a1ad4ba5c
Kyle Close <k.james.close@gmail.com>
2024-04-16 15:11:21 -0700

    save

--- src/data/carouselData.ts
+++ src/data/carouselData.ts
@@ -1,25 +1,31 @@
-import jsCarouselImg from "../img/js-carousel.svg";
-import tsCarouselImg from "../img/ts-carousel.svg";
-import reactCarouselImg from "../img/react-carousel.svg";
-import htmlCarouselImg from "../img/html-carousel.svg";
-import tailwindCarouselImg from "../img/tailwind-carousel.svg";
-import nodeCarouselImg from "../img/node-carousel.svg";
-import gitCarouselImg from "../img/git-carousel.svg";
-import mySQLCarouselImg from "../img/mySQL-carousel.svg";
-import reduxCarouselImg from "../img/redux-carousel.svg";
-import firebaseCarouselImg from "../img/firebase-carousel.svg";
+import jsCarouselImg from '../img/js-carousel.svg';
+import tsCarouselImg from '../img/ts-carousel.svg';
+import reactCarouselImg from '../img/react-carousel.svg';
+import htmlCarouselImg from '../img/html-carousel.svg';
+import tailwindCarouselImg from '../img/tailwind-carousel.svg';
+import nodeCarouselImg from '../img/node-carousel.svg';
+import gitCarouselImg from '../img/git-carousel.svg';
+import mySQLCarouselImg from '../img/mySQL-carousel.svg';
+import reduxCarouselImg from '../img/redux-carousel.svg';
+import firebaseCarouselImg from '../img/firebase-carousel.svg';
+import jestCarouselImg from '../img/jest.svg';
+import expressCarouselImg from '../img/express.svg';
+import cssCarouselImg from '../img/css.svg';
 
 const data = [
-  { src: jsCarouselImg, title: "Javascript" },
-  { src: tsCarouselImg, title: "Typescript" },
-  { src: reactCarouselImg, title: "React" },
-  { src: reduxCarouselImg, title: "Redux" },
-  { src: htmlCarouselImg, title: "HTML 5" },
-  { src: tailwindCarouselImg, title: "Tailwind" },
-  { src: nodeCarouselImg, title: "NodeJS" },
-  { src: gitCarouselImg, title: "Git" },
-  { src: mySQLCarouselImg, title: "mySQL" },
-  { src: firebaseCarouselImg, title: "Firebase" },
+  { src: jsCarouselImg, title: 'Javascript' },
+  { src: tsCarouselImg, title: 'Typescript' },
+  { src: htmlCarouselImg, title: 'HTML 5' },
+  { src: cssCarouselImg, title: 'CSS 3' },
+  { src: reactCarouselImg, title: 'React' },
+  { src: nodeCarouselImg, title: 'NodeJS' },
+  { src: expressCarouselImg, title: 'Express JS' },
+  { src: mySQLCarouselImg, title: 'mySQL' },
+  { src: jestCarouselImg, title: 'Jest' },
+  { src: reduxCarouselImg, title: 'Redux' },
+  { src: tailwindCarouselImg, title: 'Tailwind' },
+  { src: gitCarouselImg, title: 'Git' },
+  { src: firebaseCarouselImg, title: 'Firebase' },
 ];
 
 export default data;
--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,40 +1,42 @@
-import knightsDark from "../img/knights-dark.png";
-import knightsLight from "../img/knights-light.png";
+import knightsDark from '../img/knights-dark.png';
+import knightsLight from '../img/knights-light.png';
 
-import snakeDark from "../img/snake-dark.png";
-import snakeLight from "../img/snake-light.png";
+import snakeDark from '../img/snake-dark.png';
+import snakeLight from '../img/snake-light.png';
 
-import memoryDark from "../img/memory-dark.png";
-import memoryLight from "../img/memory-light.png";
+import memoryDark from '../img/memory-dark.png';
+import memoryLight from '../img/memory-light.png';
+
+import bitelogDark from '../img/laptop-bitelog-dark.png';
 
 const ProjectData = [
   {
-    name: "Memory Mix-Up",
-    techStack: ["Javascript", "HTML", "CSS"],
+    name: 'Bitelog',
+    techStack: ['React', 'Express', 'Postgres', 'Jest'],
     description:
-      "Select a theme for each new game. Navigate through levels by clicking on unique images. Be cautious: clicking an image twice will lead to defeat. Remember, images shuffle after every click.",
-    source: "https://github.com/Kyle-Close/memory-card-game",
-    live: "https://kyle-close.github.io/memory-card-game/",
-    imgDark: memoryDark,
-    imgLight: memoryLight,
-  },
-  {
-    name: "Snake",
-    techStack: ["React", "Redux", "Tailwind"],
-    description:
-      "Re-creation of the clasic game snake. Select between 3 difficulties.",
-    source: "https://github.com/Kyle-Close/snake",
-    live: "https://kyle-close.github.io/snake/",
+      'Bitelog is a dynamic web application designed to empower individuals to track and analyze their dietary habits in relation to their health. Users can create personalized lists of ingredients and meals, log dietary intakes with detailed timestamps, and record health events such as discomforts or reactions following their meals. This platform serves as a powerful tool for identifying food sensitivities, managing diets, and optimizing nutritional health.',
+    source: 'https://github.com/Kyle-Close/bitelog-client',
+    live: 'https://kyle-close.github.io/bitelog-client/',
     imgDark: snakeDark,
     imgLight: snakeLight,
   },
   {
-    name: "Knights-Travails",
-    techStack: ["Javascript", "HTML", "CSS"],
+    name: 'Memory Mix-Up',
+    techStack: ['Javascript', 'HTML', 'CSS'],
+    description:
+      'Select a theme for each new game. Navigate through levels by clicking on unique images. Be cautious: clicking an image twice will lead to defeat. Remember, images shuffle after every click.',
+    source: 'https://github.com/Kyle-Close/memory-card-game',
+    live: 'https://kyle-close.github.io/memory-card-game/',
+    imgDark: bitelogDark,
+    imgLight: bitelogDark,
+  },
+  {
+    name: 'Knights-Travails',
+    techStack: ['Javascript', 'HTML', 'CSS'],
     description:
       "A knight in chess can move to any square on the standard 8x8 chess board from any other square on the board, given enough turns. In this application, I've used my knowledge of data structures & algorithms to visualize the shortest path. Choose a starting square and ending square, then click calculate to visualize.",
-    source: "https://github.com/Kyle-Close/Knights-Travails",
-    live: "https://kyle-close.github.io/Knights-Travails/",
+    source: 'https://github.com/Kyle-Close/Knights-Travails',
+    live: 'https://kyle-close.github.io/Knights-Travails/',
     imgDark: knightsDark,
     imgLight: knightsLight,
   },
