a44d4af27cf596e65acdd9c202b8b76e4b80b60b
Kyle-Close <k.james.close@gmail.com>
2026-02-12 17:50:22 -0800

    changed base back

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: '/portfolio/',
+    base: '/',
     build: {
       outDir: 'dist'
     }
